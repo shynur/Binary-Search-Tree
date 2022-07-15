3e983f6e9a3a1dc06461722d35a6f748ab04e25b
Shynur <one.last.kiss@qq.com>
2022-07-15 11:28:26 +0800

    attach some notices to the start suggested by GPL

--- BST.hpp
+++ BST.hpp
@@ -1,3 +1,21 @@
+/* 
+ * one pattern for generating BST and some popular implementations of BBST
+ * Copyright (C) 2022 Shynur <one.last.kiss@qq.com>. 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef BST_Shynur
 #define BST_Shynur
 // *INDENT-OFF*
